d87eb5b0a077c791bc07b4ed695604364e0b4139
沈杰 <shenjie@yunhx.net>
2020-06-17 11:36:10 +0800

    fix

--- eureka-server2/src/main/java/com/shenjies88/eurekaserver2/EurekaServer2Application.java
+++ eureka-server2/src/main/java/com/shenjies88/eurekaserver2/EurekaServer2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import java.util.Random;
 @Slf4j
 @RestController
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
 public class EurekaServer2Application {
