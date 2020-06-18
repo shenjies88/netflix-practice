a88f4bb5b675361b24c29d305cd468ac254d856e
shenjies88 <1961486254@qq.com>
2020-06-18 18:20:26 +0800

    fix

--- eureka-gateway/src/main/java/com/shenjies88/eurekagateway/ErrorHandlerController.java
+++ eureka-gateway/src/main/java/com/shenjies88/eurekagateway/ErrorHandlerController.java
@@ -3,6 +3,7 @@ package com.shenjies88.eurekagateway;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.shenjies88.eurekacommon.vo.HttpResultVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author shenjies88
  */
+@Slf4j
 @RestController
 public class ErrorHandlerController implements ErrorController {
 
@@ -22,11 +24,17 @@ public class ErrorHandlerController implements ErrorController {
     @RequestMapping("/error")
     public HttpResultVo error() {
         RequestContext ctx = RequestContext.getCurrentContext();
-        ZuulException exception = (ZuulException) ctx.getThrowable();
-        if (HttpStatus.UNAUTHORIZED.value() == exception.nStatusCode) {
-            return HttpResultVo.failure(HttpStatus.UNAUTHORIZED.value(), exception.getMessage());
-        } else {
-            return HttpResultVo.failure("服务器繁忙");
+        Throwable throwable = ctx.getThrowable();
+        if (throwable == null) {
+            return HttpResultVo.failure(HttpStatus.NOT_FOUND.value(), "路径错误");
         }
+        log.error("网关异常", throwable);
+        if (throwable instanceof ZuulException) {
+            ZuulException exception = (ZuulException) throwable;
+            if (HttpStatus.UNAUTHORIZED.value() == exception.nStatusCode) {
+                return HttpResultVo.failure(HttpStatus.UNAUTHORIZED.value(), exception.getMessage());
+            }
+        }
+        return HttpResultVo.failure("服务器繁忙");
     }
 }
