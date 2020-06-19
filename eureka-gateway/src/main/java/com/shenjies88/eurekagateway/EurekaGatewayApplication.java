81a8db0fa2f2b4701980fe3edfcfeea0a4a824dd
shenjies88 <1961486254@qq.com>
2020-06-19 18:00:17 +0800

    fix

--- eureka-gateway/src/main/java/com/shenjies88/eurekagateway/EurekaGatewayApplication.java
+++ eureka-gateway/src/main/java/com/shenjies88/eurekagateway/EurekaGatewayApplication.java
@@ -2,14 +2,12 @@ package com.shenjies88.eurekagateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author shenjies88
  * @since 2020/6/18-11:19 AM
  */
-@RefreshScope
 @EnableZuulProxy
 @SpringBootApplication(scanBasePackages = {"com.shenjies88.eurekagateway", "com.shenjies88.eurekacommon.config"})
 public class EurekaGatewayApplication {
